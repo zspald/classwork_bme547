b71b420a81bec61e9a60cd72b7ba2191b50727c5
zspald <spaldingzac@gmail.com>
2022-09-05 12:30:51 -0400

    wrote basic framework for user input

---
+++ blood_calculator.py
@@ -0,0 +1,9 @@
+def interface():
+    print("My Program")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+   
+interface()
