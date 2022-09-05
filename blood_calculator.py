242f4a74801cfa0ca4d0bb576ab77934d8b6bd64
zspald <spaldingzac@gmail.com>
2022-09-05 12:46:29 -0400

    added function to receive user input for HDL value from keyboard

--- blood_calculator.py
+++ blood_calculator.py
@@ -8,5 +8,10 @@ def interface():
         choice = input("Enter your choice: ")
         if choice=='9':
             return
+
+def input_HDL():
+    hdl_in = input('Please enter HDL value: ')
+    return int(hdl_in)
+
    
 interface()
