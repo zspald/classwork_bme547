b135d1d617676a95dbf9a773f8c2ba13b64c11f1
zspald <spaldingzac@gmail.com>
2022-09-19 13:00:39 -0400

    wrote tests for check LDL function

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -10,3 +10,14 @@ def test_check_HDL(HDL_val, expected):
     result = check_HDL(HDL_val)
     assert result == expected
 
+@pytest.mark.parametrize("LDL_val, expected", 
+    [(120, "Normal"), 
+     (145, "Borderline High"), 
+     (175, "High"), 
+     (200, "Very High")],
+    ids=['Normal Test', 'Borderline High Test', 'High Test', 'Very High Test'])
+def test_check_LDL(LDL_val, expected):
+    from blood_calculator import check_LDL
+    result = check_LDL(LDL_val)
+    assert result == expected
+
