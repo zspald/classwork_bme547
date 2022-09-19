9a816b25194f71e3d5f08043ace81476f2d603a6
zspald <spaldingzac@gmail.com>
2022-09-19 13:15:55 -0400

    fixed pep8 failures in test_blood_calculator.py

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,31 +1,37 @@
 import pytest
 
-@pytest.mark.parametrize("HDL_val, expected", 
-    [(85, "Normal"), 
-     (50, "Borderline Low"), 
-     (35, "Low")],
-    ids=['Normal Test', 'Borderline Low Test', 'Low Test'])
+
+@pytest.mark.parametrize("HDL_val, expected",
+                         [(85, "Normal"),
+                          (50, "Borderline Low"),
+                          (35, "Low")],
+                         ids=['Normal Test',
+                              'Borderline Low Test', 'Low Test'])
 def test_check_HDL(HDL_val, expected):
     from blood_calculator import check_HDL
     result = check_HDL(HDL_val)
     assert result == expected
 
-@pytest.mark.parametrize("LDL_val, expected", 
-    [(120, "Normal"), 
-     (145, "Borderline High"), 
-     (175, "High"), 
-     (200, "Very High")],
-    ids=['Normal Test', 'Borderline High Test', 'High Test', 'Very High Test'])
+
+@pytest.mark.parametrize("LDL_val, expected",
+                         [(120, "Normal"),
+                          (145, "Borderline High"),
+                          (175, "High"),
+                          (200, "Very High")],
+                         ids=['Normal Test', 'Borderline High Test',
+                              'High Test', 'Very High Test'])
 def test_check_LDL(LDL_val, expected):
     from blood_calculator import check_LDL
     result = check_LDL(LDL_val)
     assert result == expected
 
-@pytest.mark.parametrize("TC_val, expected", 
-    [(180, "Normal"), 
-     (220, "Borderline High"), 
-     (250, "High")],
-    ids=['Normal Test', 'Borderline High Test', 'High Test'])
+
+@pytest.mark.parametrize("TC_val, expected",
+                         [(180, "Normal"),
+                          (220, "Borderline High"),
+                          (250, "High")],
+                         ids=['Normal Test', 'Borderline High Test',
+                              'High Test'])
 def test_check_total_cholesterol(TC_val, expected):
     from blood_calculator import check_total_cholesterol
     result = check_total_cholesterol(TC_val)
