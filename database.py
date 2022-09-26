80b3477210bd01b9281f341d9b8803c5e1e049f5
zspald <spaldingzac@gmail.com>
2022-09-26 12:50:33 -0400

    updated database from list to dictionary to hold patients

--- database.py
+++ database.py
@@ -13,10 +13,11 @@ def create_patient_entry(pt_first_name, pt_last_name, pt_id, pt_age):
 def output_database(db):
     print('##### Patient Database #####')
     for pt in db:  # iterate through patients in database
-        print("Name: {}, ID: {}, Age: {}, Tests: {}".format(get_full_name(pt),
-                                                            pt['Id'],
-                                                            pt['Age'],
-                                                            pt['Tests']))
+
+        print("Name: {}, ID: {}, Age: {}, Tests: {}".format(get_full_name(db[pt]),
+                                                            db[pt]['Id'],
+                                                            db[pt]['Age'],
+                                                            db[pt]['Tests']))
         print()  # newline
 
 
@@ -28,12 +29,8 @@ def get_full_name(pt):
 
 
 def find_pt(db, id):
-    for pt in db:  # iterate through patients
-        if pt['Id'] == id:
-            return pt  # return patient list if ID matches
-
-    print('Patient ID not in database')
-    return False
+    pt = db[id]
+    return pt
 
 
 def add_pt_test(db, pt_id, test_name, test_value):
@@ -49,23 +46,23 @@ def adult_or_minor(pt):
 
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    db = {}
+    db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
     # output_database(db)
 
     print(find_pt(db, 3))
     print()
-    add_pt_test(db, 2, 'HDL', 140)
+    add_pt_test(db, 22, 'HDL', 140)
     output_database(db)
-    add_pt_test(db, 2, 'LDL', 185)
+    add_pt_test(db, 22, 'LDL', 185)
     output_database(db)
-    add_pt_test(db, 1, 'LDL', 172)
+    add_pt_test(db, 11, 'LDL', 172)
     output_database(db)
 
-    print("Patient {} is a {}".format(get_full_name(db[2]),
-                                      adult_or_minor(db[2])))
+    # print("Patient {} is a {}".format(get_full_name(db[2]),
+    #                                   adult_or_minor(db[2])))
 
 
 if __name__ == "__main__":
