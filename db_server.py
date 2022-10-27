7ec42335dbc4a890d1199aa64782848198f4a14c
zspald <spaldingzac@gmail.com>
2022-10-27 10:23:37 -0400

    server design in-class 10/27/22

--- db_server.py
+++ db_server.py
@@ -6,12 +6,12 @@ Database format
     "id": <int>
     "blood_type": <string>
     "test_names": [<string>]
-    "test_results": [<string>]
+    "test_result": [<string>]
 }]
 """
 
 from flask import Flask, jsonify, request
-
+import logging
 
 db = []
 
@@ -37,6 +37,7 @@ def init_server():
     add_patient('Ann Ables', 1, 'A+')
     add_patient('Bob Boyles', 2, 'B+')
     # initialize logging
+    logging.basicConfig(filename='server.log')
 
 
 @app.route('/new_patient', methods=['POST'])
@@ -45,6 +46,9 @@ def post_new_patient():
     1. Receive data from post request
     2. Call other functions to do all the work
     3. Return information
+
+    NEED TO ADD DATA FORMAT IN DOCSTRINGS OF ALL FLASK HANDLERS
+
     """
     in_data = request.get_json()
     message, status_code = add_new_patient_worker(in_data)
@@ -79,7 +83,62 @@ def validate_new_patient(in_data):
     return True
 
 
-# @app.route('/add_test', methods='POST')
+@app.route('/add_test', methods=['POST'])
+def post_add_test():
+    """
+    in_data format: {
+        'id': int,
+        'test_name': str,
+        'test_result': int
+        }
+    """
+    in_data = request.get_json()
+    message, status_code = add_test_worker(in_data)
+    return message, status_code
+
+
+def add_test_worker(in_data):
+    result = validate_add_test(in_data)
+    if result is not True:
+        return result, 400
+
+    add_test(in_data['id'], in_data['test_name'], in_data['test_result'])
+    return 'Test successfully added', 200
+
+
+def validate_add_test(in_data):
+    if type(in_data) is not dict:
+        return "POST data was not a dictionary"
+
+    # check for correct keys and that keys map to correct type
+    expected_keys = ['id', 'test_name', 'test_result']
+    expected_types = [int, str, int]
+    for key, ex_type in zip(expected_keys, expected_types):
+        if key not in in_data:
+            return f'Key {key} is missing from POST data'
+        if type(in_data[key]) is not ex_type:
+            return f'Value of key {key} is not of data type {ex_type}'
+
+    # check patient is in database
+    id = in_data['id']
+    pt = find_pt(id)
+    if not pt:
+        return f'ID {id} not in patient database'
+
+    return True
+
+
+def find_pt(id):
+    for pt in db:
+        if pt['id'] == id:
+            return pt
+    return False
+
+
+def add_test(id, test_name, test_result):
+    pt = find_pt(id)
+    pt['test_name'].append(test_name)
+    pt['test_result'].append(test_result)
 
 
 # @app.route('/get_results/<patient_id>', methods=['GET'])
